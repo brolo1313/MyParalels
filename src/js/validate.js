d0ed2a20f7a49cd8cb07fa527b56bb23468d924a
brolo1313 <73553177+brolo1313@users.noreply.github.com>
2021-05-07 15:29:34 +0300

    валидация форма js и патернами

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,22 +1,97 @@
 
 const mailInput = document.getElementById('mail-input');
+const nameInput = document.getElementById('name-input');
+const span = document.querySelector('.span');
+const spanMail = document.querySelector('.span__mail');
 
-mailInput.onblur = function() {
-    if (!mailInput.value.includes('@')) { // не email
-        mailInput.classList.add('invalid');
-        mailInput.placeholder = "wrong email"; 
-    }else if (mailInput.value.includes('@')) {
-        mailInput.classList.add('done');
+
+let regName = /^[A-Za-zА-Яа-яЁё]{2,15}/;
+let regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+
+
+
+
+
+document.getElementById('submit-btn').onclick = function(e){
+  e.preventDefault();
+  if(!validate(regName, nameInput.value)){
+    notValid(nameInput, span, 'Проверка не пройдена')
+    nameInput.classList.remove('done');
+  }else if (!validate(regEmail, mailInput.value)) {
+    notValid(mailInput, span, 'Проверка не пройдена' );
+    mailInput.classList.remove('done');
+  }
+  else {
+    valid(nameInput, span, 'Проверка пройдена');
+    valid(mailInput, span, 'Проверка пройдена')
+  }
+}
+
+//функция возвращает проверку на верность
+function validate(regex, inp){
+  return regex.test(inp);
+}
+
+// валидация не пройдена
+function notValid(inp, el, mess){
+  inp.classList.add('invalid');
+ 
+  el.innerHTML = mess;
+  el.style.color = 'red';
+  
+}
+
+// валидация пройдена
+function valid(inp, el, mess){
+  inp.classList.remove('invalid');
+  inp.classList.add('done');
+  el.innerHTML = mess;
+  el.style.color = 'green';
+ 
+}
+
+
+nameInput.onblur=function(){
+  if(validate(regName, nameInput.value)){
+    nameInput.classList.remove('invalid');
+    nameInput.classList.add('done');
+   
+  }else{
     
-    }
+    nameInput.classList.add('invalid');
+    
+  }
+}
 
-  };
+mailInput.onblur=function(){
+  if(validate(regEmail, mailInput.value)){
+ 
+    mailInput.classList.add('done');
+   
+  }else{
+    mailInput.classList.remove('done');
+    mailInput.classList.add('invalid');
+  }
+}
 
 
-  mailInput.onfocus = function() {
-    if (this.classList.contains('invalid')) {
-      // удаляем индикатор ошибки, т.к. пользователь хочет ввести данные заново
-      mailInput.classList.remove('done');
-      
-    }
-  };
+
+
+// MODAL VALIDATE
+
+
+const modalName = document.getElementById('modal_name');
+const modalEmail = document.getElementById('modal_email');
+const modalPhone = document.getElementById('modal_phone');
+
+
+
+
+modalName.oninvalid = function(event) {
+  event.target.setCustomValidity('только буквы руского или латинского алфавита');
+}
+
+modalEmail.oninvalid = function(event) {
+  event.target.setCustomValidity('пример: youremail@gmail.com');
+}
+
