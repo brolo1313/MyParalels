4bd907a6eed77563662b5411cc444f13a20bff43
brolo1313 <brolo1341@gmail.com>
2022-03-20 20:20:15 +0200

    Update 2022-03-20T18:20:12.380Z

---
+++ telegram.php
@@ -0,0 +1,37 @@
+<?php
+
+/* https://api.telegram.org/bot1733019307:AAHVZjZb8mD95UCsIeqo2OrDrBMyame5qjU/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$text = $_POST['text'];
+$phone = $_POST['phone'];
+
+$token = "1733019307:AAHVZjZb8mD95UCsIeqo2OrDrBMyame5qjU";
+$chat_id = "-356258135";
+$arr = array(
+    'Имя пользователя :' => $name,
+    'Email :' => $email,
+    'ТЕКСТ :' => $text,
+    'PHONE :' => $phone
+    
+);
+foreach($arr as $key => $value) {
+    $txt .= "<b>".$key."</b> ".$value."%0A";
+  };
+
+  $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+  if ($sendToTelegram) {
+    header('Location: thank-you.html');
+    exit;
+    // echo "Спасибо";
+  } else {
+    echo "Error";
+
+
+  }
+
+
+?>
